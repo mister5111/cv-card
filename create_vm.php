88d60b9ad5dd6b37779c0e63d32b75d62e78f48a
kodi <kodi@gmail.com>
2025-03-17 15:10:12 +0200

    modified:   create_vm.php

--- create_vm.php
+++ create_vm.php
@@ -1,11 +1,23 @@
 <?php
 header('Content-Type: application/json');
 
-$password = getenv('PASSWORD');
+require __DIR__ . '/vendor/autoload.php';  
+$dotenv = Dotenv\Dotenv::createImmutable('/var/www/visit-card/');
+$dotenv->load();
 
-$escaped_password = escapeshellarg($password);
+$password = getenv('PASSWORD'); 
 
-$output = shell_exec("echo $escaped_password");
+if (!$password) {
+    echo json_encode(['error' => 'Переменная PASSWORD не найдена']);
+    exit;
+}
+
+$output = shell_exec("echo " . escapeshellarg($password));
+
+if (!$output) {
+    echo json_encode(['error' => 'shell_exec() не вернул данных']);
+    exit;
+}
 
 echo json_encode(['output' => trim($output)]);
 ?>
