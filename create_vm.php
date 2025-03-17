c0358d1f62edbe21e3138c3a3f02370b5e314768
kodi <kodi@gmail.com>
2025-03-17 14:45:54 +0200

    modified:   create_vm.php

--- create_vm.php
+++ create_vm.php
@@ -1,11 +1,7 @@
 <?php
-$config = include('/usr/secure/config.php');
-$password = escapeshellarg($config['password']);
-
 header('Content-Type: application/json');
-
-$output = shell_exec("
+$output = shell_exec('
 echo "test"
-");
-echo json_encode(['output' => trim($output)]);
+');
+echo json_encode(['output' => $output]);
 ?>
